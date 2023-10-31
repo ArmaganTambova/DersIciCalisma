99909184ab275c08d439856f0686741bf253fdef
ArmaganTambova <armagantambova@gmail.com>
2023-10-31 12:08:42 +0300

    DersIciCalisma

---
+++ StringDiziler/StringDiziler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDiziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // "string" Türü Değişken Olarak Tanımlansada Aynı Zamanda Bir Karakter Dizisidir
+            string Isim = "Armağan Tambova";
+
+            Console.WriteLine("Dizinin Eleman Sayısı = " + Isim.Length);
+            Console.WriteLine("Dizinin 2. Indexindeki Karakter = " + Isim[1]);
+
+            // Dizinin Indexlerine Karakter Atanamaz (ReadOnly) Sadece Okunabilir Alanlardır
+            //Isim[2] = "o";
+
+            // Ancak Sona Ekleme Yapılabilir
+            Isim += " & Aptal Kaan";
+            Console.WriteLine(Isim);
+        }
+    }
+}
