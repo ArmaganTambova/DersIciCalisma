538b40a6caec423ed85d9f6c2983f5a3758bfca4
ArmaganTambova <armagantambova@gmail.com>
2023-10-31 12:15:18 +0300

    DersIciCalisma

--- StringDiziler/StringDiziler/Program.cs
+++ StringDiziler/StringDiziler/Program.cs
@@ -10,18 +10,31 @@ namespace StringDiziler
     {
         static void Main(string[] args)
         {
+            #region Tanımlama
             // "string" Türü Değişken Olarak Tanımlansada Aynı Zamanda Bir Karakter Dizisidir
-            string Isim = "Armağan Tambova";
+            //string Isim = "Armağan Tambova";
 
-            Console.WriteLine("Dizinin Eleman Sayısı = " + Isim.Length);
-            Console.WriteLine("Dizinin 2. Indexindeki Karakter = " + Isim[1]);
+            //Console.WriteLine("Dizinin Eleman Sayısı = " + Isim.Length);
+            //Console.WriteLine("Dizinin 2. Indexindeki Karakter = " + Isim[1]);
 
-            // Dizinin Indexlerine Karakter Atanamaz (ReadOnly) Sadece Okunabilir Alanlardır
+            // Dizinin Indexlerine Karakter Atanamaz(ReadOnly) Sadece Okunabilir Alanlardır
             //Isim[2] = "o";
 
             // Ancak Sona Ekleme Yapılabilir
-            Isim += " & Aptal Kaan";
-            Console.WriteLine(Isim);
+            //Isim += " & Aptal Kaan";
+            //Console.WriteLine(Isim);
+            #endregion
+
+            #region İsminizi Karakterlerini Tek Tek Alarak Yazdırınız
+            //string Isim = "Armağan Tambova";
+
+            //Console.WriteLine(Isim + " Adı " + Isim.Length + " Karakterden Oluşmaktadır");
+
+            //for (int i = 0; i < Isim.Length; i++)
+            //{
+            //    Console.WriteLine(Isim[i]);
+            //}
+            #endregion
         }
     }
 }
