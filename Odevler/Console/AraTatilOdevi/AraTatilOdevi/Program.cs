44e76deaa5a6a7ee0be792a9a36ab54c59d5b2bc
ArmaganTambova <armagantambova@gmail.com>
2023-11-06 22:30:07 +0300

    DersIciCalisma

--- Odevler/Console/AraTatilOdevi/AraTatilOdevi/Program.cs
+++ Odevler/Console/AraTatilOdevi/AraTatilOdevi/Program.cs
@@ -15,9 +15,45 @@ namespace AraTatilOdevi
             string[] KitabinYazari = { "Jose Mauro De Vasconcelos", "Anthony Burgess", "Ahmet Cevizci", "Mark Wolynn", "Carl Sagan", "Akilah Azra Kohen", "Fyodor Dostoyevski", "Charles Darwin", "Charles Darwin", "Akilah Azra Kohen", "Akilah Azra Kohen", "Akilah Azra Kohen", "David Eagleman", "Miguel de Cervantes", "Tanol Türkoğlu", "Bernhard Kegel", "Stephen W. Hawking", "Stephen W. Hawking", "William Shakespeare", "Yuval Noah Harari" };
             string[] KitabinTuru = { "Roman", "Roman", "Araştırma-İnceleme", "Araştırma-İnceleme", "Bilim-Kurgu", "Roman", "Roman", "Bilim-Teknoloji-Mühendislik", "Bilim-Teknoloji-Mühendislik", "Roman", "Roman", "Roman", "Bilim-Teknoloji-Mühendislik", "Roman", "Bilim-Kurgu", "Bilim-Teknoloji-Mühendislik", "Bilim-Teknoloji-Mühendislik", "Bilim-Teknoloji-Mühendislik", "Dünya Klasikleri", "Bilim-Teknoloji-Mühendislik" };
             string[] KitabinYayinEvi = { "Can Yayınları", "İş Bankası Kültür Yayınları", "Say Yayınları", "Sola Unitas Yayınları", "Say Yayınları", "Everest Yayınları", "İş Bankası Kültür Yayınları", "Yason Yayıncılık", "Alfa Yayınları", "Everest Yayınları", "Everest Yayınları", "Everest Yayınları", "Domingo Yayınevi", "Koridor Yayınları", "Epsilon Yayınevi", "Say Yayınları", "Alfa Bilim", "Alfa Yayıncılık", "İş Bankası Kültür Yayınları", "Kolektif Kitap" };
-            double[] KitabinFiyati = { 85.00, 28.60, 133.00, 102.95, 190.00, 146.25, 56.80, 105.60, 97.20, 136.50, 90.35, 136.50, 92.14, 68.87, 117.95, 152.00, 90.35, 74.75, 13.86, 166.85};
+            double[] KitabinFiyati = { 85.00, 28.60, 133.00, 102.95, 190.00, 146.25, 56.80, 105.60, 97.20, 136.50, 90.35, 136.50, 92.14, 68.87, 117.95, 152.00, 90.35, 74.75, 13.86, 166.85 };
 
-            Console.WriteLine($"{KitapAdi.Length} {KitabinYazari.Length} {KitabinTuru.Length} {KitabinYayinEvi.Length} {KitabinFiyati.Length}");
+            string[] KitaplarinTurleri = { "Roman", "Araştırma-İnceleme", "Bilim-Kurgu", "Bilim-Teknoloji-Mühendislik", "Dünya Klasikleri" };
+            string[] KitaplarinYazarlari = { "Jose Mauro De Vasconcelos", "Anthony Burgess", "Ahmet Cevizci", "Mark Wolynn", "Carl Sagan", "Akilah Azra Kohen", "Fyodor Dostoyevski", "Charles Darwin", "David Eagleman", "Miguel de Cervantes", "Tanol Türkoğlu", "Bernhard Kegel", "Stephen W. Hawking", "William Shakespeare", "Yuval Noah Harari" };
+
+            bool Kontrol = true;
+
+            Console.WriteLine("Hoşgeldiniz");
+
+            while (Kontrol)
+            {
+                Console.WriteLine("Lütfen Kitapları Nasıl Sıralamak İstediğinizi Seçiniz\n1- Türe Göre\n2- Yazara Göre\nHangi Listeleme Yöntemini Seçmek İstersiniz?");
+                int Secim = Convert.ToInt32(Console.ReadLine());
+
+                if (Secim == 1)
+                {
+                    int sayac = 1;
+
+                    for (int i = 0; i < KitaplarinTurleri.Length; i++)
+                    {
+                        Console.WriteLine($"{sayac}-) {KitaplarinTurleri[i]}");
+                        sayac++;
+                    }
+                }
+                else if (Secim == 2)
+                {
+                    int sayac = 1;
+
+                    for (int i = 0; i < KitaplarinTurleri.Length; i++)
+                    {
+                        Console.WriteLine($"{sayac}-) {KitaplarinYazarlari[i]}");
+                        sayac++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli Bir Seçenek Girmediniz");
+                }
+            }
             #endregion
 
             #region İlaç Sistemi
