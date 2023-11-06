b5fb86440d18d6afd64306f53cf9c9f919343a28
ArmaganTambova <armagantambova@gmail.com>
2023-11-06 22:39:20 +0300

    DersIciCalisma

--- Odevler/Console/AraTatilOdevi/AraTatilOdevi/Program.cs
+++ Odevler/Console/AraTatilOdevi/AraTatilOdevi/Program.cs
@@ -20,40 +20,6 @@ namespace AraTatilOdevi
             string[] KitaplarinTurleri = { "Roman", "Araştırma-İnceleme", "Bilim-Kurgu", "Bilim-Teknoloji-Mühendislik", "Dünya Klasikleri" };
             string[] KitaplarinYazarlari = { "Jose Mauro De Vasconcelos", "Anthony Burgess", "Ahmet Cevizci", "Mark Wolynn", "Carl Sagan", "Akilah Azra Kohen", "Fyodor Dostoyevski", "Charles Darwin", "David Eagleman", "Miguel de Cervantes", "Tanol Türkoğlu", "Bernhard Kegel", "Stephen W. Hawking", "William Shakespeare", "Yuval Noah Harari" };
 
-            bool Kontrol = true;
-
-            Console.WriteLine("Hoşgeldiniz");
-
-            while (Kontrol)
-            {
-                Console.WriteLine("Lütfen Kitapları Nasıl Sıralamak İstediğinizi Seçiniz\n1- Türe Göre\n2- Yazara Göre\nHangi Listeleme Yöntemini Seçmek İstersiniz?");
-                int Secim = Convert.ToInt32(Console.ReadLine());
-
-                if (Secim == 1)
-                {
-                    int sayac = 1;
-
-                    for (int i = 0; i < KitaplarinTurleri.Length; i++)
-                    {
-                        Console.WriteLine($"{sayac}-) {KitaplarinTurleri[i]}");
-                        sayac++;
-                    }
-                }
-                else if (Secim == 2)
-                {
-                    int sayac = 1;
-
-                    for (int i = 0; i < KitaplarinTurleri.Length; i++)
-                    {
-                        Console.WriteLine($"{sayac}-) {KitaplarinYazarlari[i]}");
-                        sayac++;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Geçerli Bir Seçenek Girmediniz");
-                }
-            }
             #endregion
 
             #region İlaç Sistemi
