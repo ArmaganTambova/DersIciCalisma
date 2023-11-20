041803e1d3905e45c8a96161b23f7f75de13d780
ArmaganTambova <armagantambova@gmail.com>
2023-11-20 15:52:16 +0300

    DersIciCalisma

---
+++ NesneYonelimliProgramlama/NesneYonelimliProgramlama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesneYonelimliProgramlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Nesne Yönelimli Programlama Nedir
+            /*
+            Object Oriented Programming
+            * Kısaca Her İşlevin Nesneler Olarak Soyutlandığı Programlama Yaklaşımıdır
+            * Değişkenlerin Dağınık Bir Şekilde Tutulması Yerine Nesneler Aracılığıyla Tutulmasını Sağlar
+            * Gerçek Hayatın Simülize Edilmesi Olarak Tanımlanabilir
+            * Sınıf Olarak "Tanımlanan" Yapıların "Nesnelleştirilmesidir"
+            */
+            #endregion
+        }
+    }
+}
