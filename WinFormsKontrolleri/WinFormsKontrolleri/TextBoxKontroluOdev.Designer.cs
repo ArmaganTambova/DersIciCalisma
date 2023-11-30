df7792f9b5f3073bca323727753c8a4235f4a9eb
ArmaganTambova <armagantambova@gmail.com>
2023-11-30 23:08:34 +0300

    DersIciCalisma

--- WinFormsKontrolleri/WinFormsKontrolleri/TextBoxKontroluOdev.Designer.cs
+++ WinFormsKontrolleri/WinFormsKontrolleri/TextBoxKontroluOdev.Designer.cs
@@ -48,12 +48,17 @@
             this.LBL_Sinif = new System.Windows.Forms.Label();
             this.CB_Sinif = new System.Windows.Forms.ComboBox();
             this.CB_Sube = new System.Windows.Forms.ComboBox();
+            this.TB_Ilce = new System.Windows.Forms.TextBox();
+            this.LBL_Ilce = new System.Windows.Forms.Label();
+            this.TB_Il = new System.Windows.Forms.TextBox();
+            this.LBL_Il = new System.Windows.Forms.Label();
             this.GB_Bilgi.SuspendLayout();
             this.SuspendLayout();
             // 
             // TB_Okul
             // 
-            this.TB_Okul.Location = new System.Drawing.Point(93, 222);
+            this.TB_Okul.BackColor = System.Drawing.Color.SeaShell;
+            this.TB_Okul.Location = new System.Drawing.Point(93, 274);
             this.TB_Okul.Name = "TB_Okul";
             this.TB_Okul.Size = new System.Drawing.Size(261, 20);
             this.TB_Okul.TabIndex = 28;
@@ -62,7 +67,7 @@
             // 
             this.LBL_Okul.AutoSize = true;
             this.LBL_Okul.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Okul.Location = new System.Drawing.Point(12, 222);
+            this.LBL_Okul.Location = new System.Drawing.Point(12, 274);
             this.LBL_Okul.Name = "LBL_Okul";
             this.LBL_Okul.Size = new System.Drawing.Size(50, 20);
             this.LBL_Okul.TabIndex = 26;
@@ -70,7 +75,8 @@
             // 
             // TB_Adres
             // 
-            this.TB_Adres.Location = new System.Drawing.Point(93, 116);
+            this.TB_Adres.BackColor = System.Drawing.Color.SeaShell;
+            this.TB_Adres.Location = new System.Drawing.Point(93, 168);
             this.TB_Adres.Multiline = true;
             this.TB_Adres.Name = "TB_Adres";
             this.TB_Adres.Size = new System.Drawing.Size(261, 100);
@@ -80,7 +86,7 @@
             // 
             this.LBL_Adres.AutoSize = true;
             this.LBL_Adres.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Adres.Location = new System.Drawing.Point(12, 116);
+            this.LBL_Adres.Location = new System.Drawing.Point(12, 168);
             this.LBL_Adres.Name = "LBL_Adres";
             this.LBL_Adres.Size = new System.Drawing.Size(58, 20);
             this.LBL_Adres.TabIndex = 24;
@@ -88,8 +94,8 @@
             // 
             // BTN_Gonder
             // 
-            this.BTN_Gonder.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.BTN_Gonder.Location = new System.Drawing.Point(369, 222);
+            this.BTN_Gonder.BackColor = System.Drawing.SystemColors.Window;
+            this.BTN_Gonder.Location = new System.Drawing.Point(369, 274);
             this.BTN_Gonder.Name = "BTN_Gonder";
             this.BTN_Gonder.Size = new System.Drawing.Size(218, 20);
             this.BTN_Gonder.TabIndex = 23;
@@ -102,7 +108,7 @@
             this.GB_Bilgi.Controls.Add(this.LBL_Bilgi);
             this.GB_Bilgi.Location = new System.Drawing.Point(369, 12);
             this.GB_Bilgi.Name = "GB_Bilgi";
-            this.GB_Bilgi.Size = new System.Drawing.Size(218, 204);
+            this.GB_Bilgi.Size = new System.Drawing.Size(218, 256);
             this.GB_Bilgi.TabIndex = 22;
             this.GB_Bilgi.TabStop = false;
             this.GB_Bilgi.Text = "Öğrenci Bilgileri:";
@@ -117,6 +123,7 @@
             // 
             // MTB_Iban
             // 
+            this.MTB_Iban.BackColor = System.Drawing.Color.SeaShell;
             this.MTB_Iban.Location = new System.Drawing.Point(168, 90);
             this.MTB_Iban.Mask = "TR99-99999-999999-999999-9999-99";
             this.MTB_Iban.Name = "MTB_Iban";
@@ -135,6 +142,7 @@
             // 
             // MTB_Telefon
             // 
+            this.MTB_Telefon.BackColor = System.Drawing.Color.SeaShell;
             this.MTB_Telefon.Location = new System.Drawing.Point(168, 64);
             this.MTB_Telefon.Mask = "(999) 000-0000";
             this.MTB_Telefon.Name = "MTB_Telefon";
@@ -153,6 +161,7 @@
             // 
             // TB_Soyisim
             // 
+            this.TB_Soyisim.BackColor = System.Drawing.Color.SeaShell;
             this.TB_Soyisim.Location = new System.Drawing.Point(93, 38);
             this.TB_Soyisim.Name = "TB_Soyisim";
             this.TB_Soyisim.Size = new System.Drawing.Size(261, 20);
@@ -170,6 +179,7 @@
             // 
             // TB_Isim
             // 
+            this.TB_Isim.BackColor = System.Drawing.Color.SeaShell;
             this.TB_Isim.Location = new System.Drawing.Point(93, 12);
             this.TB_Isim.Name = "TB_Isim";
             this.TB_Isim.Size = new System.Drawing.Size(261, 20);
@@ -189,7 +199,7 @@
             // 
             this.LBL_Bolum.AutoSize = true;
             this.LBL_Bolum.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Bolum.Location = new System.Drawing.Point(12, 248);
+            this.LBL_Bolum.Location = new System.Drawing.Point(12, 300);
             this.LBL_Bolum.Name = "LBL_Bolum";
             this.LBL_Bolum.Size = new System.Drawing.Size(64, 20);
             this.LBL_Bolum.TabIndex = 26;
@@ -197,7 +207,8 @@
             // 
             // TB_Bolum
             // 
-            this.TB_Bolum.Location = new System.Drawing.Point(93, 248);
+            this.TB_Bolum.BackColor = System.Drawing.Color.SeaShell;
+            this.TB_Bolum.Location = new System.Drawing.Point(93, 300);
             this.TB_Bolum.Name = "TB_Bolum";
             this.TB_Bolum.Size = new System.Drawing.Size(261, 20);
             this.TB_Bolum.TabIndex = 28;
@@ -206,7 +217,7 @@
             // 
             this.LBL_Sinif.AutoSize = true;
             this.LBL_Sinif.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Sinif.Location = new System.Drawing.Point(12, 275);
+            this.LBL_Sinif.Location = new System.Drawing.Point(12, 327);
             this.LBL_Sinif.Name = "LBL_Sinif";
             this.LBL_Sinif.Size = new System.Drawing.Size(48, 20);
             this.LBL_Sinif.TabIndex = 29;
@@ -214,6 +225,7 @@
             // 
             // CB_Sinif
             // 
+            this.CB_Sinif.BackColor = System.Drawing.Color.SeaShell;
             this.CB_Sinif.FormattingEnabled = true;
             this.CB_Sinif.Items.AddRange(new object[] {
             "1",
@@ -228,13 +240,14 @@
             "10",
             "11",
             "12"});
-            this.CB_Sinif.Location = new System.Drawing.Point(93, 275);
+            this.CB_Sinif.Location = new System.Drawing.Point(93, 327);
             this.CB_Sinif.Name = "CB_Sinif";
             this.CB_Sinif.Size = new System.Drawing.Size(120, 21);
             this.CB_Sinif.TabIndex = 30;
             // 
             // CB_Sube
             // 
+            this.CB_Sube.BackColor = System.Drawing.Color.SeaShell;
             this.CB_Sube.FormattingEnabled = true;
             this.CB_Sube.Items.AddRange(new object[] {
             "A",
@@ -266,17 +279,57 @@
             "V",
             "Y",
             "Z"});
-            this.CB_Sube.Location = new System.Drawing.Point(234, 274);
+            this.CB_Sube.Location = new System.Drawing.Point(234, 326);
             this.CB_Sube.Name = "CB_Sube";
             this.CB_Sube.Size = new System.Drawing.Size(120, 21);
             this.CB_Sube.TabIndex = 31;
+            // 
+            // TB_Ilce
+            // 
+            this.TB_Ilce.BackColor = System.Drawing.Color.SeaShell;
+            this.TB_Ilce.Location = new System.Drawing.Point(93, 142);
+            this.TB_Ilce.Name = "TB_Ilce";
+            this.TB_Ilce.Size = new System.Drawing.Size(261, 20);
+            this.TB_Ilce.TabIndex = 34;
+            // 
+            // LBL_Ilce
+            // 
+            this.LBL_Ilce.AutoSize = true;
+            this.LBL_Ilce.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_Ilce.Location = new System.Drawing.Point(12, 142);
+            this.LBL_Ilce.Name = "LBL_Ilce";
+            this.LBL_Ilce.Size = new System.Drawing.Size(40, 20);
+            this.LBL_Ilce.TabIndex = 32;
+            this.LBL_Ilce.Text = "İlçe:";
+            // 
+            // TB_Il
+            // 
+            this.TB_Il.BackColor = System.Drawing.Color.SeaShell;
+            this.TB_Il.Location = new System.Drawing.Point(93, 116);
+            this.TB_Il.Name = "TB_Il";
+            this.TB_Il.Size = new System.Drawing.Size(261, 20);
+            this.TB_Il.TabIndex = 35;
+            // 
+            // LBL_Il
+            // 
+            this.LBL_Il.AutoSize = true;
+            this.LBL_Il.Font = new System.Drawing.Font("SoDo Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_Il.Location = new System.Drawing.Point(12, 116);
+            this.LBL_Il.Name = "LBL_Il";
+            this.LBL_Il.Size = new System.Drawing.Size(23, 20);
+            this.LBL_Il.TabIndex = 33;
+            this.LBL_Il.Text = "İl:";
             // 
             // TextBoxKontroluOdev
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(599, 359);
+            this.Controls.Add(this.TB_Ilce);
+            this.Controls.Add(this.LBL_Ilce);
+            this.Controls.Add(this.TB_Il);
+            this.Controls.Add(this.LBL_Il);
             this.Controls.Add(this.CB_Sube);
             this.Controls.Add(this.CB_Sinif);
             this.Controls.Add(this.LBL_Sinif);
@@ -326,6 +379,10 @@
         private System.Windows.Forms.Label LBL_Sinif;
         private System.Windows.Forms.ComboBox CB_Sinif;
         private System.Windows.Forms.ComboBox CB_Sube;
+        private System.Windows.Forms.TextBox TB_Ilce;
+        private System.Windows.Forms.Label LBL_Ilce;
+        private System.Windows.Forms.TextBox TB_Il;
+        private System.Windows.Forms.Label LBL_Il;
     }
 }
 
--- WinFormsKontrolleri/WinFormsKontrolleri/TextBoxKontroluOdev.cs
+++ WinFormsKontrolleri/WinFormsKontrolleri/TextBoxKontroluOdev.cs
@@ -19,33 +19,34 @@ namespace WinFormsKontrolleri
 
         private void BTN_Gonder_Click(object sender, EventArgs e)
         {
-            string Isim = TB_Isim.Text;
-            string Soyisim = TB_Soyisim.Text;
-            string Telefon = MTB_Telefon.Text;
-            string Iban = MTB_Iban.Text;
-            string Adres = TB_Adres.Text;
-            string Okul = TB_Okul.Text;
-            string Bolum = TB_Bolum.Text;
-
-            string Sinif = CB_Sinif.Text;
-            string Sube = CB_Sube.Text;
-
-            if (Sinif != null &&  Sube != null)
+            if (!string.IsNullOrEmpty(TB_Isim.Text) && !string.IsNullOrEmpty(TB_Soyisim.Text) && !string.IsNullOrEmpty(MTB_Telefon.Text) && !string.IsNullOrEmpty(MTB_Iban.Text) && !string.IsNullOrEmpty(TB_Il.Text) && !string.IsNullOrEmpty(TB_Ilce.Text) && !string.IsNullOrEmpty(TB_Adres.Text) && !string.IsNullOrEmpty(TB_Okul.Text) && !string.IsNullOrEmpty(TB_Bolum.Text) && !string.IsNullOrEmpty(CB_Sinif.Text) && !string.IsNullOrEmpty(CB_Sube.Text))
             {
-                string GSinif = $"{CB_Sinif.Text} / {CB_Sube.Text}";
+                string Isim = TB_Isim.Text;
+                string Soyisim = TB_Soyisim.Text;
+                string Telefon = MTB_Telefon.Text;
+                string Iban = MTB_Iban.Text;
+                string Adres = $"\n{TB_Il.Text}/{TB_Ilce.Text}\n{TB_Adres.Text}\n";
+                string Okul = TB_Okul.Text;
+                string Bolum = TB_Bolum.Text;
+                string Sinif = $"{CB_Sinif.Text} / {CB_Sube.Text}";
 
-                if (Isim != null && Soyisim != null && Telefon != null && Iban != null && Adres != null && Okul != null && Bolum != null)
-                {
-                    LBL_Bilgi.Text = $"{Isim} {Soyisim}\n{Telefon}\n{Iban}\n{Adres}\n{Okul}\n{Bolum}\n{GSinif}";
-                }
-                else
-                {
-                    MessageBox.Show("Lütfen Tüm Alanları Doldurduğunuzdan Emin Olunuz", "Hata");
-                }
+                LBL_Bilgi.Text = $"{Isim} {Soyisim}\n{Telefon}\n{Iban}\n{Adres}\n{Okul}\n{Bolum}\n{Sinif}";
+
+                TB_Isim.Text = null;
+                TB_Soyisim.Text = null;
+                MTB_Telefon.Text = null;
+                MTB_Iban.Text = null;
+                TB_Il.Text = null;
+                TB_Ilce.Text = null;
+                TB_Adres.Text = null;
+                TB_Okul.Text = null;
+                TB_Bolum.Text = null;
+                CB_Sinif.Text = null;
+                CB_Sube.Text = null;
             }
             else
             {
-                MessageBox.Show("Lütfen Tüm Alanları Doldurduğunuzdan Emin Olunuz", "Hata");
+                MessageBox.Show("Lütfen Boş Alan Bırakmayın" , "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
