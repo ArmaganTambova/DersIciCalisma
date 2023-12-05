751bb2aece6e1f6a877381be83cb6eb6c9d21e05
ArmaganTambova <armagantambova@gmail.com>
2023-12-05 21:58:27 +0300

    DersIciCalisma

---
+++ Metotlar/Metotlar/Cirak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class Cirak
+    {
+        //public void SelamVer()
+        //{
+            //Console.WriteLine("Ustamın Selamı Var");
+        //}
+
+        // Parametre Alan Metot
+        // Parametre Nedir? Metodun Çağırıldığı Alandan Netoda Kullanması İçin Gönderilen Veriyi Tutan Değişkene Parametre Denir
+        //public void IsmeSelamVer(string Isim)
+        //{
+            //Console.WriteLine($"{Isim} Abi Ustamın Selamı Var");
+        //}
+    }
+}
---
+++ Metotlar/Metotlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tanımlama
+            // Metotlar / Functions
+            // Ana Programı Küçük Parçalara Ayırarak İhtiyaç Duyduğunda Çalıştırmak Üzere Programladığımız Küçük Program Parçalarıdır
+            // Metotlar Sınıfların İçerisinde Tanımlanabilir
+            #endregion
+
+            #region Çıkar Sınıfı
+            // "Cirak" Sınıfından Referans Alan Nesne Var Edildi
+            //Cirak cirak = new Cirak();
+
+            // Program "SelamVer" İsimli Metot'dan Devam Eder
+            //cirak.SelamVer();
+            // Metot İşlemi Bitince Bu Alandan Devam Eder
+
+            //cirak.IsmeSelamVer(); // Parametre Alan Metotlar, Parametre Olarak Veri Gönderilmeden Çalışamazlar
+
+            //cirak.IsmeSelamVer("Armağan");
+            #endregion
+        }
+    }
+}
