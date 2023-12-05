2c332f849d45134222fa039e15af7a71e7ac645e
ArmaganTambova <armagantambova@gmail.com>
2023-12-05 22:32:45 +0300

    DersIciCalisma

---
+++ Odevler/Console/MetotlarOdev/MetotlarOdev/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotlarOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PeriyodikTablo periyodikTablo = new PeriyodikTablo();
+
+            //Console.WriteLine(periyodikTablo.AtomAdlari.Length);
+            //Console.WriteLine(periyodikTablo.AtomSembolleri.Length);
+            //Console.WriteLine(periyodikTablo.KutleNumaralari.Length);
+            //Console.WriteLine(periyodikTablo.Ketegoriler.Length);
+
+            for (int i = 0; i < periyodikTablo.AtomAdlari.Length; i++)
+            {
+                Console.WriteLine($"{periyodikTablo.AtomAdlari[i]} ({periyodikTablo.AtomSembolleri[i]}) - {periyodikTablo.KutleNumaralari[i]} / {periyodikTablo.Ketegoriler[i]}");
+            }
+        }
+    }
+}
