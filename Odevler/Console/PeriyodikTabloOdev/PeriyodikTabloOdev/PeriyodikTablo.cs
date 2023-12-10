47e20079001ae2f92d7d8ace2f8571d2de9e92fe
ArmaganTambova <armagantambova@gmail.com>
2023-12-10 19:05:40 +0300

    DersIciCalisma

---
+++ Odevler/Console/PeriyodikTabloOdev/PeriyodikTabloOdev/PeriyodikTablo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriyodikTabloOdev
+{
+    public class PeriyodikTablo
+    {
+        public string AtomAdi;
+        public string AtomSembolu;
+        public double KutleNumarasi;
+        public string Kategorisi;
+    }
+}
