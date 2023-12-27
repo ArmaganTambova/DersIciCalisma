b9aae702bf584a4f5aec0767a43846e5c4336fff
ArmaganTambova <armagantambova@gmail.com>
2023-12-27 23:12:59 +0300

    DersIciCalisma

--- Odevler/Console/ErdalBakkalGenericCollections/ErdalBakkalGenericCollections/Program.cs
+++ Odevler/Console/ErdalBakkalGenericCollections/ErdalBakkalGenericCollections/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ErdalBakkalGenericCollections
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
