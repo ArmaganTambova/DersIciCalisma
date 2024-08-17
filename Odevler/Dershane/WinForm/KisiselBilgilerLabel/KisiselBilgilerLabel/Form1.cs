a89ba80842f4d595b7402cb68b231180aee4ebf3
ArmaganTambova <armagantambova@gmail.com>
2024-08-18 00:56:33 +0300

    DersIciCalisma

---
+++ Odevler/Dershane/WinForm/KisiselBilgilerLabel/KisiselBilgilerLabel/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KisiselBilgilerLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Label LBL1 = new Label();
+            LBL1.Text = "E-Posta = armagantambova@gmail.com";
+            LBL1.Location = new Point(10, 70);
+            LBL1.AutoSize = true;
+            this.Controls.Add(LBL1);
+
+            Label LBL2 = new Label();
+            LBL2.Text = "Telefon: +90 536 401 08 26";
+            LBL2.Location = new Point(10, 100);
+            LBL2.AutoSize = true;
+            this.Controls.Add(LBL2);
+        }
+    }
+}
