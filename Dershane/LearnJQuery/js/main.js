50ce04b22c0f96d738da453b6335650740b3c26c
ArmaganTambova <armagantambova@gmail.com>
2025-04-27 12:50:39 +0300

    DersIciCalisma

---
+++ Dershane/LearnJQuery/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("button").click(function()
+    {
+        /*Etiket Seçici*/
+        /*$("p").hide();*/ 
+
+        /*id Seçici*/
+        /*$("#teori").hide();*/ 
+
+        /*sinif Seçici*/
+        /*$(".deneme").hide();*/ 
+
+        /* Tüm Kırmızı sınıfını kullanana paragraflar*/
+        /*$("p.kirmizi").hide();*/
+
+        /*Sayfadaki ilk Paragraf*/
+        /*$("p:first").hide();*/
+
+       /* $("p:even").hide();*/
+        $("p:odd").hide();
+    });
+});
